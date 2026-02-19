ce3ce91e8af61b34560fa131697243370940dbe3
Mahmoud El-Saeed <mahmoud.elsaeed.cs@gmail.com>
2026-02-19 17:00:25 +0200

    Refactors lifespan function to improve Qdrant client initialization and collection setup
    Improves Qdrant client initialization and collection setup

--- main.py
+++ main.py
@@ -3,20 +3,31 @@ from pymongo import AsyncMongoClient
 from contextlib import asynccontextmanager
 from qdrant_client import AsyncQdrantClient
 from src.app.routes import data
-from src.helper.config import get_settings , Settings
+from src.app.database.qdrantdb.QdrantdbModel import QdrantdbModel
+from src.helper.config import get_settings, Settings
+from src.app.routes.chat import register_chat_routes
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     settings: Settings = get_settings()
     app.mongo_conn = AsyncMongoClient(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
-    app.qdrant_client = AsyncQdrantClient(
-        url=settings.URL_QDRANT
+    app.qdrant_client = AsyncQdrantClient(url=settings.URL_QDRANT)
+    qdrant_model = QdrantdbModel(app.qdrant_client)
+    await qdrant_model.create_collection_if_not_exists(
+        collection_name=settings.COLLECTION_APP_NAME,
+        vector_size=settings.EMBED_MODEL_SIZE
     )
-    
+    await qdrant_model.create_collection_if_not_exists(
+        collection_name=settings.COLLECTION_CHATS_HISTORY_NAME,
+        vector_size=settings.EMBED_MODEL_SIZE
+    )
+    register_chat_routes(app)
+
     yield
-    await app.mongo_conn.close() 
+    await app.mongo_conn.close()
     await app.qdrant_client.close()
+
 
 app = FastAPI(lifespan=lifespan)
 
